1b6f3be69dd83e4476f028cd64ec4de85b4f5d1f
Michael Moor <michael.moor@goltfisch.de>
2018-10-21 22:32:06 +0200

    Update logs collection and methods

--- imports/api/logs/logs-methods.js
+++ imports/api/logs/logs-methods.js
@@ -3,7 +3,16 @@ import { Meteor } from 'meteor/meteor';
 import { Logs } from './logs.js'
 
 Meteor.methods({
-    'Logs.insert' (log) {
+    'Logs.insert' (action, type) {
+
+        const log = {
+            action: action,
+            type: type,
+            user: Meteor.user().name + ' ' + Meteor.user().username,
+            time: moment(new Date()).format('H:m:s'),
+            createdAt: moment(new Date()).format('DD.MM.YYYY')
+        }
+
         return Logs.insert(log);
     }
 });
