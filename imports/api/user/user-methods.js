9426f3db64ce90e028a24e98cf6a7cdb282d2888
Michael Moor <Michael.moor@goltfisch.de>
2019-02-04 20:31:21 +0100

    Update user and userRole methods
    Closes:#security-update

--- imports/api/user/user-methods.js
+++ imports/api/user/user-methods.js
@@ -3,189 +3,127 @@ import { Accounts } from "meteor/accounts-base";
 
 Meteor.methods({
     "User.insert"(user) {
-        const currentUser = Meteor.user();
-        const isAdmin =
-            currentUser &&
-            currentUser.userRole &&
-            currentUser.userRole == "admin";
-
-        if (isAdmin) {
-            user.notifications = [
-                "onCreatedRequest",
-                "onApprovedRequest",
-                "onPurchasedApproval",
-                'onDeclinedRequest',
-                'onShelvedRequest',
-                "onCompletedOrder",
-                "onChangedUserRole"
-            ];
-
-            const createUser = Accounts.createUser(user);
-            const currentUser = Meteor.user();
-
-            const newLog = {
-                action:
-                    "Admin " +
-                    currentUser.name +
-                    " hat den Benutzer " +
-                    user.name +
-                    " angelegt.",
-                type: "info"
-            };
-
-            Meteor.call("Logs.insert", newLog.action, newLog.type);
-
-            return createUser;
-        } else {
-            throw new Meteor.Error("not-authorized", "You are not authorized!");
+        if (!this.userId || Meteor.user().userRole != 'admin') {
+            throw new Meteor.Error('not-authorized');
         }
+
+        const currentUser = Meteor.user();
+
+        user.notifications = [
+            "onCreatedRequest",
+            "onApprovedRequest",
+            "onPurchasedApproval",
+            'onDeclinedRequest',
+            'onShelvedRequest',
+            "onCompletedOrder",
+            "onChangedUserRole"
+        ];
+
+        const createUser = Accounts.createUser(user);
+
+        const newLog = {
+            action: "Admin " + currentUser.name + " hat den Benutzer " + user.name + " angelegt.",
+            type: "info"
+        };
+
+        Meteor.call("Logs.insert", newLog.action, newLog.type);
+
+        return createUser;
     },
     "User.update"(user) {
+        if (!this.userId || Meteor.user().userRole != 'admin' || this.userId != user._id) {
+            throw new Meteor.Error('not-authorized');
+        }
+
         const currentUser = Meteor.user();
 
-        const isAdmin =
-            currentUser &&
-            currentUser.userRole &&
-            currentUser.userRole == "admin";
-        const isUserHimself = currentUser && currentUser._id == user._id;
-
-        if (isAdmin || isUserHimself) {
-            Meteor.users.update(user._id, {
-                $set: {
-                    username: user.username,
-                    name: user.name,
-                    //active: user.active,
-                    userRole: user.userRole
-                }
-            });
-
-            if (user.oldRole) {
-                if (user.oldRole != user.userRole) {
-                    delete user.oldRole;
-
-                    const admins = Meteor.users
-                        .find({ userRole: "admin" })
-                        .fetch();
-
-                    let adminIds = [];
-
-                    if (admins) {
-                        admins.forEach(element => {
-                            adminIds.push(element._id);
-                        });
-                    }
-
-                    emailTo = {
-                        targetUser: user._id,
-                        userRole: user.userRole,
-                        admins: adminIds,
-                        user: user._id,
-                        templateNames: ["userRoleMail"]
-                    };
-
-                    Meteor.call(
-                        "MailService.renderEmail",
-                        emailTo,
-                        (error, result) => {
-                            if (error) {
-                                console.log("Fehler: ", error);
-                            }
-                        }
-                    );
-
-                    const newLog = {
-                        action:
-                            "Admin " +
-                            currentUser.name +
-                            " hat die Rolle von " +
-                            user.name +
-                            " in " +
-                            user.userRole +
-                            " geändert.",
-                        type: "info"
-                    };
-
-                    Meteor.call("Logs.insert", newLog.action, newLog.type);
-                }
-            } else {
+        Meteor.users.update(user._id, {
+            $set: {
+                username: user.username,
+                name: user.name,
+                //active: user.active,
+                userRole: user.userRole
             }
-        } else {
-            throw new Meteor.Error("not-authorized");
+        });
+
+        if (user.oldRole) {
+            if (user.oldRole != user.userRole) {
+                delete user.oldRole;
+
+                const admins = Meteor.users.find({ userRole: "admin" }).fetch();
+                let adminIds = [];
+
+                if (admins) {
+                    admins.forEach(element => {
+                        adminIds.push(element._id);
+                    });
+                }
+
+                emailTo = {
+                    targetUser: user._id,
+                    userRole: user.userRole,
+                    admins: adminIds,
+                    user: user._id,
+                    templateNames: ["userRoleMail"]
+                };
+
+                Meteor.call("MailService.renderEmail", emailTo, (error, result) => {
+                    if (error) {
+                        console.log("Fehler: ", error);
+                    }
+                });
+
+                const newLog = {
+                    action: "Admin " + currentUser.name + " hat die Rolle von " + user.name + " in " + user.userRole + " geändert.",
+                    type: "info"
+                };
+
+                Meteor.call("Logs.insert", newLog.action, newLog.type);
+            }
         }
     },
     "User.delete"(user) {
-        const currentUser = Meteor.user();
-        const isAdmin =
-            currentUser &&
-            currentUser.userRole &&
-            currentUser.userRole == "admin";
-
-        if (isAdmin) {
-            const deleteResult = Meteor.users.remove({ _id: user._id });
-
-            const newLog = {
-                action:
-                    "Admin " +
-                    currentUser.name +
-                    " hat den Benutzer " +
-                    user.name +
-                    " gelöscht.",
-                type: "info"
-            };
-
-            Meteor.call("Logs.insert", newLog.action, newLog.type);
-
-            return deleteResult;
-        } else {
-            throw new Meteor.Error("not-authorized", "You are not authorized!");
+        if (!this.userId || Meteor.user().userRole != 'admin') {
+            throw new Meteor.Error('not-authorized');
         }
+
+        const currentUser = Meteor.user();
+        const deleteResult = Meteor.users.remove({ _id: user._id });
+
+        const newLog = {
+            action: "Admin " + currentUser.name + " hat den Benutzer " + user.name + " gelöscht.",
+            type: "info"
+        };
+
+        Meteor.call("Logs.insert", newLog.action, newLog.type);
+
+        return deleteResult;
     },
     "User.updatePassword"(user) {
+        if (!this.userId || Meteor.user().userRole != 'admin' || this.userId != user._id) {
+            throw new Meteor.Error('not-authorized');
+        }
+
         const currentUser = Meteor.user();
 
-        const isAdmin =
-            currentUser &&
-            currentUser.userRole &&
-            currentUser.userRole == "admin";
+        Accounts.setPassword(user._id, user.password);
 
-        const isUserHimself = currentUser && currentUser._id == user._id;
+        const newLog = {
+            action: "Admin " + currentUser.name + " hat das Passwort von " + user.name + " geändert.",
+            type: "info"
+        };
 
-        if (isAdmin || isUserHimself) {
-            Accounts.setPassword(user._id, user.password);
-
-            const newLog = {
-                action:
-                    "Admin " +
-                    currentUser.name +
-                    " hat das Passwort von " +
-                    user.name +
-                    " geändert.",
-                type: "info"
-            };
-
-            Meteor.call("Logs.insert", newLog.action, newLog.type);
-        } else {
-            throw new Meteor.Error("not-authorized");
-        }
+        Meteor.call("Logs.insert", newLog.action, newLog.type);
     },
     "User.updateSettings"(user) {
-        const currentUser = Meteor.user();
-
-        const isAdmin =
-            currentUser &&
-            currentUser.userRole &&
-            currentUser.userRole == "admin";
-
-        const isUserHimself = currentUser && currentUser._id == user._id;
-
-        if (isAdmin || isUserHimself) {
-            Meteor.users.update(user._id, {
-                $set: {
-                    ...user
-                }
-            });
-        } else {
-            throw new Meteor.Error("not-authorized");
+        if (!this.userId || Meteor.user().userRole != 'admin' || this.userId != user._id) {
+            throw new Meteor.Error('not-authorized');
         }
+
+        Meteor.users.update(user._id, {
+            $set: {
+                ...user
+            }
+        });
     }
 });
--- imports/api/user/userRoles/userRoles-methods.js
+++ imports/api/user/userRoles/userRoles-methods.js
@@ -4,11 +4,19 @@ import { UserRoles } from './userRoles.js'
 
 Meteor.methods({
     'UserRoles.insert' (role) {
+        if (!this.userId || Meteor.user().userRole == 'user') {
+            throw new Meteor.Error('not-authorized');
+        }
+
         UserRoles.schema.validate(role);
 
         return UserRoles.insert(role);
     },
     'UserRoles.update'(role) {
+        if (!this.userId || Meteor.user().userRole == 'user') {
+            throw new Meteor.Error('not-authorized');
+        }
+        
         UserRoles.schema.validate(role);
         
         return UserRoles.update(role._id, { $set: role });
