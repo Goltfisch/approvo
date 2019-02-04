9f567febf439cbc35081ce5948dbacd6ce3a9465
Michael Moor <Michael.moor@goltfisch.de>
2019-02-04 20:31:01 +0100

    Update tags methods

--- imports/api/tags/tags-methods.js
+++ imports/api/tags/tags-methods.js
@@ -4,6 +4,10 @@ import { Tags } from './tags.js'
 
 Meteor.methods({
     'Tags.insert' (tag) {
+        if (!this.userId || Meteor.user().userRole == 'user') {
+            throw new Meteor.Error('not-authorized');
+        }
+
         tag.createdAt = new Date();
 
         Tags.schema.validate(tag);
@@ -11,9 +15,17 @@ Meteor.methods({
         return Tags.insert(tag);
     },
     'Tags.update' (tag) {
+        if (!this.userId || Meteor.user().userRole == 'user') {
+            throw new Meteor.Error('not-authorized');
+        }
+
         return Tags.update(tag._id, { $set: tag });
     },
     'Tags.remove' (tagId) {
+        if (!this.userId || Meteor.user().userRole == 'user') {
+            throw new Meteor.Error('not-authorized');
+        }
+        
         return Tags.remove(tagId);
     }
 });
