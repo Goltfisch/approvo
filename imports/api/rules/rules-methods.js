9a5b0c6f7653b449d742c6a67084c55e8fb0df2f
Michael Moor <Michael.moor@goltfisch.de>
2019-02-04 20:30:53 +0100

    Update rule methods

--- imports/api/rules/rules-methods.js
+++ imports/api/rules/rules-methods.js
@@ -4,6 +4,10 @@ import { Rules } from './rules.js'
 
 Meteor.methods({
     'Rules.insert' (rule) {
+        if (!this.userId || Meteor.user().userRole == 'user') {
+            throw new Meteor.Error('not-authorized');
+        }
+
         rule.createdAt = new Date();
 
         Rules.schema.validate(rule);
@@ -11,9 +15,17 @@ Meteor.methods({
         return Rules.insert(rule);
     },
     'Rules.update' (rule) {
+        if (!this.userId || Meteor.user().userRole == 'user') {
+            throw new Meteor.Error('not-authorized');
+        }
+
         return Rules.update(rule._id, { $set: rule });
     },
     'Rules.remove' (ruleId) {
+        if (!this.userId || Meteor.user().userRole == 'user') {
+            throw new Meteor.Error('not-authorized');
+        }
+        
         return Rules.remove(ruleId);
     }
 });
