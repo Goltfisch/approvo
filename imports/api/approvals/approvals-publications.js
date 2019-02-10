d4b02e7a2b2980c7185dbe1d2e5de60dd3dd3d29
Michael Moor <Michael.moor@goltfisch.de>
2019-02-10 18:14:41 +0100

    Update publications
    Closes:#Show-only-users-approvals

--- imports/api/approvals/approvals-publications.js
+++ imports/api/approvals/approvals-publications.js
@@ -6,6 +6,37 @@ Meteor.publish('dashboard.approvals', function (searchQuery, currentPage) {
     if(!this.userId) {
         throw new Meteor.Error('not authorized', 'You are not authorized!');
     }
+    const currentUser = Meteor.user();
+
+    if(currentUser.userRole != 'user') {
+        let regex = new RegExp(searchQuery, 'i');
+
+        let q = {
+            $or: [
+                { 'name': regex },
+                { 'amount': regex },
+                { 'reason': regex },
+                { 'link': regex },
+                { 'state': regex },
+            ]
+        };
+
+        let p = {
+            sort: { date: -1 },
+        };
+
+        Counts.publish(this, 'dashboardApprovalsCount', Approvals.find(q, p));
+
+        p.limit = 25;
+        
+        currentPage--;
+
+        if(currentPage > 0) {
+            p.skip = currentPage * p.limit;
+        }
+
+        return Approvals.find(q, p);
+    }
 
     let regex = new RegExp(searchQuery, 'i');
 
@@ -18,6 +49,8 @@ Meteor.publish('dashboard.approvals', function (searchQuery, currentPage) {
             { 'state': regex },
         ]
     };
+
+    q.owner = currentUser._id;
 
     let p = {
         sort: { date: -1 },
