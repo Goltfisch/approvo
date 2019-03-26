caa061d1a49cf672a2925e3c603f3d775e85aabf
Michael Moor <Michael.moor@goltfisch.de>
2019-03-26 12:03:11 +0100

    Increase versionNumber

--- imports/rainbow-ui/Footer.js
+++ imports/rainbow-ui/Footer.js
@@ -10,7 +10,7 @@ export default class Footer extends Component {
                     &copy; 2018 Goltfisch GmbH
                 </div>
                 <div className='version-block'>
-                    Approvo version 1.3.4
+                    Approvo version 1.3.5
                 </div>
                 <div className='message-block'>
                     Made with <i className="fas fa-heart"></i> in Paderborn
